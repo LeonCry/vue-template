84cd3c5445985f3078aee8785f899613f460464a
voidis <lbh_ldu@outlook.com>
2025-08-29 13:22:34 +0800

    fetch ky请求库 替换 XHR axios请求库

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
-import axios from 'axios';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import Request from './utils/request';
 
 declare const __APP_VERSION__: string;
 const app = createApp(App).use(router).use(createPinia());
 app.mount('#app');
-
+const req = new Request({ prefixUrl: `/${import.meta.env.VITE_APP_ROUTER_PREFIX}` });
 router.beforeEach(() => {
   const { DEV } = import.meta.env;
   if (DEV) return;
-  const pre = import.meta.env.VITE_APP_ROUTER_PREFIX;
-  axios.get(`/${pre}/version.json?t=${Date.now()}`)
+  req.GET<{ version: string }>(`/version.json?t=${Date.now()}`)
     .then(({ data }) => {
       const version = data.version;
       if (!version || typeof __APP_VERSION__ === 'undefined') return;
